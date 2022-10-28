f1eac249ab6e51f4c4d5b0bf43de143694b0b086
astoppop <michealhuang7@gmail.com>
2022-10-28 07:54:05 -0400

    a

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
+import { StatusBar } from 'expo-status-bar'
 import { ScrollView, StyleSheet, Text, View, Button, Image, TouchableOpacity, Modal, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -204,41 +203,6 @@ const styles = StyleSheet.create({
         elevation: 10,
         alignItems: 'center',
     },
-=======
-import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
-
-const App = () => {
-  return (
-    <ScrollView contentContainerStyle={styles.scrollcontainer}>
-      <Button
-      onPress={() => {
-        alert('You tapped the button!');
-      }}
-      title="Press Me"
-    />
-  </ScrollView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollcontainer: {
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'stretch',
-    flexProperty: 'flexWrap',
-  },
-  button: {
-    backgroundColor: 'orange',
-    alignItems: 'center',
-  },
->>>>>>> refs/remotes/origin/main
-});
+};
 
 export default App;
